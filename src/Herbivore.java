67df100ffb9ae6b9d76d6fba34591c3f51b79d60
Yurii <0939864774z@gmail.com>
2022-05-26 01:03:06 +0300

    change Herbivore

--- src/Herbivore.java
+++ src/Herbivore.java
@@ -2,7 +2,7 @@ public abstract class Herbivore extends Animal {
 
     public void eat(Food food) {
         System.out.print("I'm " + name + ". ");
-        if (food != this && food instanceof Plant) {
+        if (food instanceof Plant) {
             System.out.println("Yummy!");
         } else {
             System.out.println("I don't eat that!");
